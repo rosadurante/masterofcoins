9534f973e33d934474755462236cb10bad2c2e95
Rosa Durante <me@rosadurante.com>
2014-06-30 22:41:07 +0100

    Added comments, added more validations to different special situations. Renamed variable

--- src/js/changeMachine.js
+++ src/js/changeMachine.js
@@ -15,26 +15,51 @@ define('changeMachine', ['underscore'], function (_) {
 
             parse: function () {
                 var amount = this.amount;
+
+                /** Fixing some special cases when the user uses "£" character at the beginning:
+                 *  - Without decimals (without '.'): "£3", "£10".
+                 *  - With a '.' as last character of the number: "£5.", "£1.p"
+                 *  - With a '.00' as last characteres of the number: "£1.00", "£2.00p"
+                 */
+
+                if (_.indexOf(amount, '£') !== -1) {
+                    if (_.indexOf(amount, '.') === -1) {
+                        amount += '00';
+                    } else if (_.contains(_.last(amount, 2), '.') || _.contains(_.last(amount, 1), '.')) {
+                        amount = amount.replace('.', '') + '00';
+                    } else if (_.indexOf(amount, '.00') !== -1 && _.indexOf(amount, '.00') + 3 === amount.length) {
+                        amount = amount.replace('.', '');
+                    }
+                }
+
                 amount = amount.replace('£', '').replace('p', '');
                 amount = parseFloat(parseFloat(amount).toFixed(2));
 
-                return (amount - parseInt(amount, 10) > 0) ? parseInt(amount * 100, 10) : amount;
+                /** Avoiding bad round from javascript 
+                    parseInt(amount * 100, 10) --> 2.07 = 206 
+                    parseInt((amount * 1000) / 10, 10) --> 2.07 = 207 */
+
+                return (amount - parseInt(amount, 10) > 0) ? parseInt((amount * 1000) / 10, 10) : amount;
             },
 
             getCoins: function () {
-                var rest, coins = [], amount = this.parse();
+                var remaining,
+                    coins = [],
+                    amount = this.parse();
 
                 _.each([200, 100, 50, 20, 10, 5, 2], function (valueCoin) {
-                    rest = amount % valueCoin;
-                    if (rest < amount) {
+                    remaining = amount % valueCoin;
+
+                    if (remaining < amount) {
                         coins.push({
                             'valueCoin': valueCoin,
-                            'amountCoins': (amount - rest) / valueCoin
+                            'amountCoins': (amount - remaining) / valueCoin
                         });
-
-                        amount = rest;
+                        amount = remaining;
                     }
                 });
+
+                /** If there is still some amount remaining, there would be 1p coins */
 
                 if (amount) {
                     coins.push({
