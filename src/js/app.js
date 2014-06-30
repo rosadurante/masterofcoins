72f536acd805e9f35fe65c0ad41941c2b865e4a6
Rosa Durante <me@rosadurante.com>
2014-06-30 18:08:06 +0100

    Overwriting event behaviour to submit and clicks

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,29 @@
 /* globals define */
 'use strict';
 
-define('app', ['underscore', 'domReady!'], function () {
-	console.log('JS - initialized');
+define('app', ['domReady!'], function (document) {
+    
+    var formElement = document.getElementById('input-form'),
+        amountInputElement = document.getElementById('amount'),
+        submitButton = document.getElementById('submit-form'),
+        resetButton = document.getElementById('reset-form'),
+        resultElement = document.getElementById('output-form'),
+
+        reset = function () {
+            amountInputElement.value = '';
+            resultElement.innerHTML = '';
+        },
+
+        submit = function (e) {
+            e.preventDefault();
+
+            console.log('Submitted');
+        };
+
+
+    submitButton.addEventListener('click', submit, false);
+    resetButton.addEventListener('click', reset, false);
+    formElement.addEventListener('submit', submit, false);
+
+    reset();
 });
