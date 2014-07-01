3f81bf8b707c1248cb2c9388f0a11fa16f8c870f
Rosa Durante <me@rosadurante.com>
2014-07-01 11:24:06 +0100

    Improving parse float number. Updated comments

--- src/js/changeMachine.js
+++ src/js/changeMachine.js
@@ -17,34 +17,28 @@ define('changeMachine', ['underscore'], function (_) {
                 var amount = this.amount;
 
                 /** Fixing some special cases when the user uses "£" character at the beginning:
-                 *  - Without decimals (without '.'): "£3", "£10".
-                 *  - With a '.' as last character of the number: "£5.", "£1.p"
-                 *  - With a '.0' as last character of the number: "£2.0", £1.0p"
-                 *  - With a '.00' as last characteres of the number: "£1.00", "£2.00p"
+                 *  - Using "£" at the beginning and without decimals (without '.'): "£3", "£10".
+                 *  -- Those need to be converted to float: "3.00", "10.00"
+                 *  - Not using "£" at the beginning and not having a '.': "345p", "23"
+                 *  -- Those need to be converted to float: "3.45", "0.23"
                  */
 
-                if (_.indexOf(amount, '£') !== -1) {
-                    if (amount.indexOf('.') === -1) {
-                        amount += '00';
-                    } else if (amount.indexOf('.p') !== -1 || _.contains(_.last(amount, 1), '.')) {
-                        amount = amount.replace('.', '') + '00';
-                    } else if ((amount.indexOf('.0') !== -1 && amount.indexOf('.0') + 2 === amount.length) ||
-                        (amount.indexOf('.0p') !== -1)) {
-                        amount = amount.replace('.', '') + '0';
-                    } else if ((amount.indexOf('.00') !== -1 && amount.indexOf('.00') + 3 === amount.length) ||
-                        (amount.indexOf('.00p') !== -1)) {
-                        amount = amount.replace('.', '');
-                    }
+                if (amount.indexOf('£') !== -1 && amount.indexOf('.') === -1) {
+                    amount += '.00';
+                }
+
+                if (amount.indexOf('£') === -1 && amount.indexOf('.') === -1) {
+                    amount = (parseInt(amount.replace('p', ''), 10) / 100).toString();
                 }
 
                 amount = amount.replace('£', '').replace('p', '');
-                amount = parseFloat(parseFloat(amount).toFixed(2));
 
-                /** Avoiding bad round from javascript 
-                    parseInt(amount * 100, 10) --> 2.07 = 206 
-                    parseInt((amount * 1000) / 10, 10) --> 2.07 = 207 */
+                /** Avoiding bad calculations (round)
+                    parseFloat(parseFloat(amount).toFixed(2)) --> 4.99 = 5
+                    parseInt(parseFloat(amount).toFixed(2).replace('.',''), 10) --> 4.99 = 499 */
 
-                return (amount - parseInt(amount, 10) > 0) ? parseInt((amount * 1000) / 10, 10) : amount;
+                amount = parseInt(parseFloat(amount).toFixed(2).replace('.', ''), 10);
+                return amount;
             },
 
             getCoins: function () {
